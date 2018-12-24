bccea23038c8cf94b180f558f2f41d5fd647daa1
Paloma Velazquez <paloma.velazquez@gmail.com>
2018-12-23 18:01:52 -0800

    Create hotel information bar, add drop-down menu & icons

--- client/hotelHeader.jsx
+++ client/hotelHeader.jsx
@@ -7,7 +7,11 @@ class Header extends React.Component {
 
 	render() {
 
-		var city = "San Francisco"
+		var city = "San Francisco";
+		var hotel = "Omni Hotel";
+		var avgrat = "4.5";
+		var ranking = "3";
+		var total = "100";
 
 		return (
 			<div>
@@ -25,14 +29,79 @@ class Header extends React.Component {
 					</div>
 				</div>
 				<nav id="global-nav">
-					<a href="#">Travel Feed: {city}</a>
-					<a href="#">Hotels</a>
-					<a href="#">Things to Do</a>
-					<a href="#">Restaurants</a>
-					<a href="#">Flights</a>
-					<a href="#">Vacation Rentals</a>
-					<a href="#">Shopping</a>
+					<ul>
+						<li>
+							<a href="#">Travel Feed: {city}</a>
+							<div className="tray">
+								<ul>
+									<li><a href="#">{city} Tourism</a></li>
+									<li><a href="#">{city} Hotels</a></li>
+									<li><a href="#">{city} Bed and Breakfast</a></li>
+									<li><a href="#">{city} Vacation Packages</a></li>
+									<li><a href="#">{city} Vacation Rentals</a></li>
+									<li><a href="#">Flights to {city}</a></li>
+									<li><a href="#">{city} Restaurants</a></li>
+									<li><a href="#">Things to Do in {city}</a></li>
+									<li><a href="#">{city} Shopping</a></li>
+									<li><a href="#">{city} Travel Forum</a></li>
+									<li><a href="#">{city} Photos</a></li>
+									<li><a href="#">{city} Map</a></li>
+									<li><a href="#">{city} Travel Guide</a></li>
+								</ul>
+							</div>
+						</li>
+						<li id="global-nav-selected">
+							<a href="#">Hotels</a>
+							<div className="tray">
+								<ul>
+									<li><a href="#">All {city} Hotels</a></li>
+									<li><a href="#">{city} Hotel Deals</a></li>
+									<li><a href="#">Last Minute Hotel Deals in {city}</a></li>
+									<li><a href="#">Hotels near {hotel}</a></li>
+								</ul>
+							</div>
+						</li>
+						<li>
+							<a href="#">Things to Do</a>
+							<div className="tray">
+								<ul>
+									<li><a href="#">All things to do in {city}</a></li>
+									<li><a href="#">Things to do near {hotel}</a></li>
+								</ul>
+							</div>
+						</li>
+						<li>
+							<a href="#">Restaurants</a>
+							<div className="tray">
+								<ul>
+									<li><a href="#">All {city} Restaurants</a></li>
+									<li><a href="#">Restaurants near {hotel}</a></li>
+								</ul>
+							</div>
+						</li>
+						<li>
+							<a href="#">Flights</a>
+						</li>
+						<li>
+							<a href="#">Vacation Rentals</a>
+						</li>
+						<li>
+							<a href="#">Shopping</a>
+						</li>
+					</ul>
 				</nav>
+				<div id="hotel-basics">
+					<h1>{hotel}</h1>
+					<div id="rating-container">
+						<span id="rating">{avgrat} average rating</span>
+						<span>#{ranking} of {total} Hotels in {city}</span>
+					</div>
+					<div id="contact-container">
+						<span id="address">91702 Heathcote Branch, {city} CA</span>
+						<span id="phone">818-726-5064</span>
+						<span id="website"><a href="#">Visit Hotel Website</a></span>
+ 					</div>
+				</div>
 			</div>
 		)
 	}
